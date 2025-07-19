977f453e201613bc14055f43cccdc37cf7932149
Teguh Bagas Mardiansyah <116806781+TeguhBagasM@users.noreply.github.com>
2025-07-20 00:32:30 +0300

    chore: initialize backend

---
+++ app/Models/Transaction.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    protected $fillable = [
+        'name',
+        'category_id',
+        'date',
+        'amount',
+        'note',
+        'image',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
