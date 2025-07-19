46698e6a9b009d1f2fd2c706068f88381397cbd0
Teguh Bagas Mardiansyah <116806781+TeguhBagasM@users.noreply.github.com>
2025-07-20 01:22:37 +0300

    feat: add widgets chart to dashboard

--- app/Filament/Widgets/WidgetExpenseChart.php
+++ app/Filament/Widgets/WidgetExpenseChart.php
@@ -2,16 +2,36 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\Transaction;
 use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
 
 class WidgetExpenseChart extends ChartWidget
 {
-    protected static ?string $heading = 'Chart';
+    protected static ?string $heading = 'Pengeluaran Chart';
 
     protected function getData(): array
     {
+        $data = Trend::model(Transaction::class)
+            ->query(
+                Transaction::expenses()
+            )
+            ->between(
+                start: now()->startOfYear(),
+                end: now()->endOfYear(),
+            )
+            ->perDay()
+            ->sum('amount');
+
         return [
-            //
+            'datasets' => [
+                [
+                    'label' => 'Pengeluaran perhari',
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => $data->map(fn (TrendValue $value) => $value->date),
         ];
     }
 
--- app/Filament/Widgets/WidgetIncomeChart.php
+++ app/Filament/Widgets/WidgetIncomeChart.php
@@ -2,16 +2,36 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\Transaction;
 use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
 
 class WidgetIncomeChart extends ChartWidget
 {
-    protected static ?string $heading = 'Chart';
+    protected static ?string $heading = 'Pemasukan Chart';
 
     protected function getData(): array
     {
+        $data = Trend::model(Transaction::class)
+            ->query(
+                Transaction::incomes()
+            )
+            ->between(
+                start: now()->startOfYear(),
+                end: now()->endOfYear(),
+            )
+            ->perDay()
+            ->sum('amount');
+
         return [
-            //
+            'datasets' => [
+                [
+                    'label' => 'Pemasukan perhari',
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => $data->map(fn (TrendValue $value) => $value->date),
         ];
     }
 
