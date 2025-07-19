18aecaf21cd7a5e641b63903bc82c2acda38e32a
Teguh Bagas Mardiansyah <116806781+TeguhBagasM@users.noreply.github.com>
2025-07-20 00:38:03 +0300

    feat: improve dashboard ui

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\Transaction;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -9,8 +10,14 @@ class StatsOverview extends BaseWidget
 {
     protected function getStats(): array
     {
+        $pemasukan = Transaction::where('is_expense', false)->sum('amount');
+        $pengeluaran = Transaction::where('is_expense', true)->sum('amount');
+        $selisih = $pemasukan - $pengeluaran;
         return [
-            //
+            Stat::make('Total Pemasukan', 'Rp. ' . $pemasukan),
+            Stat::make('Total Pengeluaran', 'Rp. ' . $pengeluaran),
+            Stat::make('Selisih', 'Rp. ' . $selisih),
+
         ];
     }
 }
